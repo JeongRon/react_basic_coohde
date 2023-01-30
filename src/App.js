c9ce9886e4c1e855ffa091c319087c957eb4e838
JeongRon <wjdfhs@gmail.com>
2023-01-30 20:57:58 +0900

    1. 컴퍼넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import "./App.css";
 
+function Header() {
+  return (
+    <a href="/">
+      <h2>WEB</h2>
+    </a>
+  );
+}
+function Nav() {
+  return (
+    <ol>
+      <li>
+        <a href="/html">html</a>
+      </li>
+      <li>
+        <a href="/css">css</a>
+      </li>
+      <li>
+        <a href="/js">js</a>
+      </li>
+    </ol>
+  );
+}
+function Article() {
+  return (
+    <div>
+      <h3>WEB</h3>
+      <p>Web descriptions</p>
+    </div>
+  );
+}
 function App() {
-  return;
+  return (
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  );
 }
 
 export default App;
